21e9e2e753e6877570916fb35a9a194151d0595c
Alexander Sinitsyn <coder27sinitsyn@gmail.com>
2026-02-01 16:56:15 +0300

    lesson #14.1
    #time 1h
    1) добавил теаорию за прошлую неделю

--- 014.ts
+++ 014.ts
@@ -1,5 +1,47 @@
 export {}
 
+// 014 3.7 distributive conditional types
+// У условного оператора есть особое поведение c union типами
+
+export {}
+
+type Union = 'id' | 'value'
+
+type WrapId<T> = T extends 'id' ? { id: T } : T
+
+// Здесь должно быть просто 'id' | 'value' так как
+// union assignable типу если все элементы assignable типу
+// но! 'value' assignable
+// И по сути мы должны уйти в false ветку, но не тут то было!
+type R = WrapId<'id' | 'value'>
+
+// Но получился тип { id: 'id' } | 'value' !!!!
+// получается он для каждого значения union прогоняет значения, потом их объединяет и возвращает такой же union
+
+
+
+// c never у нас не может что-то пересикаться поэтому он будет откидываться
+// фильтровать union лучше с тернарными операциями
+type OnlyObjectsWithType<T> = T extends { type: string } ? T : never
+
+type Res = OnlyObjectsWithType<number | { type: 'delete' } | { type: 'create' }>
+
+// пример с массивом
+type WrapInArray1<T> = T[]
+
+type R1 = WrapInArray1<number | string>
+
+type WrapInArray2<T> = T extends unknown ? T[] : never
+
+type R2 = WrapInArray2<number | string>
+
+// пример как отменить conditional distributive
+type WrapId123<T> = [T] extends ['id'] ? { id: T } : T
+
+type R123 = WrapId123<'id' | 'value' | 'res'>
+type Value123 = WrapId123<never>
+type Value1234 = WrapId123<any>
+
 // Домашняя работа
 
 // Задание #1
