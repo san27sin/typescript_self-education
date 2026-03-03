e59cb8ee98912b671b47f1375624350f1c44a8f8
Alexander Sinitsyn <coder27sinitsyn@gmail.com>
2026-03-03 10:11:13 +0300

    #lesson 17.2
    #time 1h 10m
    1) теория

--- 017.ts
+++ 017.ts
@@ -1,31 +1,62 @@
-// 018 3.10 recursion
+// 017 3.92 опциональность и mapped types
 
 export {}
 
-// мы можем использовать typescript типы для создания рекурсивных структур
-
-type Tree<G> = {
-  value: G
-  children: Tree<G>[]
+type Obj = {
+  a: number
+  b?: number
 }
 
-const tree: Tree<number> = {
-  value: 1,
-  children: [{ value: 2, children: [] }]
+type JustMap<T> = {
+  [K in keyof T]: T[K]
 }
 
-// мы можем это использовать для рекурсивных дженериков
+// 1. mapped types сохраняет опциональность
+type R1 = JustMap<Obj>
+// ^?type R1 = { a: number; b?: number | undefined }
 
-type MyAwaited<T> = T extends Promise<infer V> ? V : T
+// 2. Опциональность определяется на основании keyof T
 
-type Res1 = MyAwaited<Promise<Promise<Promise<number>>>>
-
-// и для рекурсивного обхода вложенных объектов
-
-type DeepReadonly<T> = {
-  readonly [K in keyof T]: DeepReadonly<T[K]>
+type Mp<T, V> = {
+  [K in keyof T]: V[K & keyof V]
 }
 
-type Res2 = DeepReadonly<{ value: { title: string } }>
+type R2 = Mp<{ name?: string }, { name: number }>
+// ^?type R2 = { name?: number | undefined }
 
-// 
+
+// 3. опциональность и union с undefined
+// пришпендюривается с верху после обработки значения
+
+type Mp2<T> = {
+  [K in keyof T]: Exclude<T[K], undefined>
+}
+
+type R3 = Mp2<{ name?: string }>
+// ^?type R3 = { name?: string | undefined }
+
+// 4. Мерджинг объектов с сохранением опциональности
+// если мерджить обязательное поле и необязательное, то результирующее будет обязательное, а если оба необязательные то добавится undefined
+
+type Simplify<T> = {
+  [K in keyof T]: T[K]
+}
+
+type R41 = Simplify<{ name: string } & { name?: string }>;
+// { name: string }
+
+type R42 = Simplify<{ name?: string } & { name?: string }>;
+// { name?: string | undefined }
+
+type R43 = Simplify<{ name: string } & { name: number }>;
+// { name: never }
+
+// решение задачи по мерджу из предыдущего урока
+type Mp3<T, V> = {
+  [K in keyof (T & V)]: K extends keyof V ? V[K] : T[K & keyof T]
+}
+
+type R44 = Mp3<{ name?: string }, { name?: number }>
+// ^?type R44 = { name?: number | undefined }
+type R45 = Mp3<{ name?: string }, { name: number }>
+// ^?type R45 = { name: number }
---
+++ 018.ts
@@ -0,0 +1,31 @@
+// 018 3.10 recursion
+
+export {}
+
+// мы можем использовать typescript типы для создания рекурсивных структур
+
+type Tree<G> = {
+  value: G
+  children: Tree<G>[]
+}
+
+const tree: Tree<number> = {
+  value: 1,
+  children: [{ value: 2, children: [] }]
+}
+
+// мы можем это использовать для рекурсивных дженериков
+
+type MyAwaited<T> = T extends Promise<infer V> ? V : T
+
+type Res1 = MyAwaited<Promise<Promise<Promise<number>>>>
+
+// и для рекурсивного обхода вложенных объектов
+
+type DeepReadonly<T> = {
+  readonly [K in keyof T]: DeepReadonly<T[K]>
+}
+
+type Res2 = DeepReadonly<{ value: { title: string } }>
+
+//
