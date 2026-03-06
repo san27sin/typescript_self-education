7915fbc93f6cf91fd9ef7bf875933363910bb311
Alexander Sinitsyn <coder27sinitsyn@gmail.com>
2026-03-06 09:12:37 +0300

    #lesson 17.3
    #time 42m
    1) теория

--- 017.ts
+++ 017.ts
@@ -40,7 +40,7 @@ type R3 = Mp2<{ name?: string }>
 
 type Simplify<T> = {
   [K in keyof T]: T[K]
-}
+} & {}
 
 type R41 = Simplify<{ name: string } & { name?: string }>;
 // { name: string }
@@ -60,3 +60,40 @@ type R44 = Mp3<{ name?: string }, { name?: number }>
 // ^?type R44 = { name?: number | undefined }
 type R45 = Mp3<{ name?: string }, { name: number }>
 // ^?type R45 = { name: number }
+
+// 5. Сохранение опциональности не работает, если мы берем union ключей
+// union c undefined получается из-за обращения к опциональному полю
+
+type Mp4<T, V> = {
+  [K in keyof T | keyof V]: K extends keyof V ? V[K] : T[K & keyof T]
+}
+
+type R51 = Mp4<{ name?: string }, { value?: number }>
+
+// 6. Мы можем этим воспользоваться,
+// что бы убрать опциональность без -?. В редких кейсах это нужно
+
+// В чем проблема -?. Оно удаляет | undefined даже если этого не нужно
+type Mp51<T> = {
+  [K in keyof T]-?: T[K] | undefined
+}
+
+type R61 = Mp51<{ name?: string | undefined; value: string | undefined }>
+// ^? type R61 = { name: string; value: string | ...
+
+// Попробуем воспользоваться хаком из прошлого
+// не работает так как ts схлопывает union
+type Mp52<T> = {
+  [K in keyof T | keyof {}]: T[K & keyof T];
+}
+
+type R62 = Mp52<{ name?: string }>
+// ^?type R62 = { name?: string | undefined }
+
+// Вот так работает
+type Mp53<T, O = {}> = {
+  [K in keyof T | keyof O]: T[K & keyof T]
+}
+
+type R63 = Mp53<{ name?: string, value: string | undefined }>
+// ^?type R63 = { name: string | undefined; valu...
