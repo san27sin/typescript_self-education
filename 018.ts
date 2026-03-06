444f4754737e0b0aa57bdcf20241c33edac5f2c4
Alexander Sinitsyn <coder27sinitsyn@gmail.com>
2026-03-06 09:56:53 +0300

    # lesson 18.1
    #time 45m
    1) теория
    2) начал делать дз

--- 018.ts
+++ 018.ts
@@ -28,4 +28,44 @@ type DeepReadonly<T> = {
 
 type Res2 = DeepReadonly<{ value: { title: string } }>
 
-//
+// Но мы можем использовать это для создания конкретных таких циклов!
+
+type Reverse<Tuple extends unknown[]> = Tuple extends [
+  infer First,
+  ...infer Tail,
+]
+  ? [...Reverse<Tail>, First]
+  : []
+
+type Ten = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+type Res4 = Reverse<Ten>
+
+// По умолчанию глубина рекурсии 50
+// @ts-expect-error
+type Res5 = Reverse<[...Ten, ...Ten, ...Ten, ...Ten, ...Ten, 1]>
+
+// На хвостовую рекурсию ограничение 1000
+type CreateTuple<
+  Num extends number,
+  Tuple extends unknown[] = [],
+> = Tuple['length'] extends Num ? Tuple : CreateTuple<Num, [...Tuple, unknown]>
+
+type Res3 = CreateTuple<999>
+
+// задание 1
+
+type DeepRequired<T> = {
+  [K in keyof T]-?: DeepRequired<T>
+}
+
+type Res123 = DeepRequired<{
+  value?: {
+    title?: string
+  }
+}> // { value: { title: string } }
+
+const res1: Res123 = {
+  value: {
+    title: 'Title'
+  }
+}
