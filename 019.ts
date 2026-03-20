f26248a836e71cfea2f0937e5543b7653ebd7642
Alexander Sinitsyn <coder27sinitsyn@gmail.com>
2026-03-20 10:00:31 +0300

    lesson #19.1
    #time 40m
    1) сделал add и с помощью ai

--- 019.ts
+++ 019.ts
@@ -1,5 +1,11 @@
 export {}
 
+type Todo = {
+  id: number,
+  test: string,
+  completed: boolean
+}
+
 type TodoList = [
   {
     id: 1,
@@ -8,8 +14,12 @@ type TodoList = [
   }
 ]
 
-type AddItem<TodoList, Id, Text> = TodoList | Id | Text
-type RemoveItem<TodoList, Id> = TodoList | Id
+type AddItem<TL extends unknown[], Id extends number, Text extends string> = [...TL, { id: Id, text: Text, completed: false }]
+
+type AddItem2 = AddItem<TodoList, 2, 'привет'>
+
+type RemoveItem<TodoList extends Todo[], Id extends number> = TodoList | Id
+
 type UpdateText<TodoList, Id, Text> = TodoList | Id
 type ToggleText<TodoList, Id> = TodoList | Id
 type FindById<TodoList, Id> = TodoList | Id
