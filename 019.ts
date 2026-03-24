c43d31c3acfc9fc862c36e959a2c730d37df5d01
Alexander Sinitsyn <coder27sinitsyn@gmail.com>
2026-03-24 10:46:10 +0300

    lesson #19.2
    #time 1h 30m
    1) RemoveItem - решил с помощью deepsick
    2) UpdateText - решил сам, но нейронка навела на мысль аккумулировать массив объектов

--- 019.ts
+++ 019.ts
@@ -2,7 +2,7 @@ export {}
 
 type Todo = {
   id: number,
-  test: string,
+  text: string,
   completed: boolean
 }
 
@@ -11,6 +11,16 @@ type TodoList = [
     id: 1,
     text: 'Create something',
     completed: false
+  },
+  {
+    id: 2,
+      text: 'iiiiiiiiiiiii',
+    completed: true
+  },
+  {
+    id: 3,
+      text: 'lllllllllllllllll',
+    completed: false
   }
 ]
 
@@ -18,9 +28,28 @@ type AddItem<TL extends unknown[], Id extends number, Text extends string> = [..
 
 type AddItem2 = AddItem<TodoList, 2, 'привет'>
 
-type RemoveItem<TodoList extends Todo[], Id extends number> = TodoList | Id
+type RemoveItem<TL extends Todo[] | Todo, Id extends number> = TL extends [infer First, ...infer Rest]
+  ? First extends Todo
+    ? First['id'] extends Id
+      ? RemoveItem<Rest, Id>
+      : [First, ...RemoveItem<Rest, Id>]
+    : never
+  : TL extends [] ? [] : never
 
-type UpdateText<TodoList, Id, Text> = TodoList | Id
+type RemoveItem2 = RemoveItem<TodoList, 2>
+
+type UpdateText<TL extends Todo[], Id extends number, Text extends string> = TL extends [infer First, ...infer Rest]
+  ? First extends Todo
+    ? First['id'] extends Id
+      ? [{
+        [K in keyof First]: K extends 'text' ? Text : First[K]
+      }, ...UpdateText<Rest, Id, Text>]
+      : [First, ...UpdateText<Rest, Id, Text>]
+    : never
+  : [] // базовый случай - пустой кортеж
+
+type UpdateText2 = UpdateText<TodoList, 3, 'changed'>
+
 type ToggleText<TodoList, Id> = TodoList | Id
 type FindById<TodoList, Id> = TodoList | Id
 type FilterBy<TodoList, Key, Value> = TodoList | Key | Value
