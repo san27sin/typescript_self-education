0a8a618cdbc10e2b158a052b0f6f266ea84a4ca5
Alexander <92460691+san27sin@users.noreply.github.com>
2026-03-25 09:48:04 +0300

    lesson #19.3
    #time 55m
    1) toggleText - сделал сам
    2) findById - сам сделал
    3) filterBy - сам сделал

--- 019.ts
+++ 019.ts
@@ -9,17 +9,17 @@ type Todo = {
 type TodoList = [
   {
     id: 1,
-    text: 'Create something',
+    text: 'первый',
     completed: false
   },
   {
     id: 2,
-      text: 'iiiiiiiiiiiii',
+      text: 'второй',
     completed: true
   },
   {
     id: 3,
-      text: 'lllllllllllllllll',
+    text: 'третий',
     completed: false
   }
 ]
@@ -50,6 +50,36 @@ type UpdateText<TL extends Todo[], Id extends number, Text extends string> = TL 
 
 type UpdateText2 = UpdateText<TodoList, 3, 'changed'>
 
-type ToggleText<TodoList, Id> = TodoList | Id
-type FindById<TodoList, Id> = TodoList | Id
-type FilterBy<TodoList, Key, Value> = TodoList | Key | Value
+type ToggleText<TL extends Todo[], Id extends number> = TL extends [infer First, ...infer Rest]
+    ? First extends Todo
+        ? First['id'] extends Id
+            ? [{
+              [K in keyof First]: K extends 'completed'
+                  ? First['completed'] extends true ? false : true
+                  : First[K]
+            }, ...ToggleText<Rest, Id>]
+            : [First, ...ToggleText<Rest, Id>]
+        : never
+    : []
+
+type ToggleText2 = ToggleText<TodoList, 3>
+
+type FindById<TL extends Todo[], Id extends number> = TL extends [infer First, ...infer Rest]
+    ? First extends Todo
+        ? First['id'] extends Id
+            ? First
+            : FindById<Rest, Id>
+        : never
+    : never
+
+type FindById2 = FindById<TodoList, 3>
+
+type FilterBy<TL extends Todo[], Key extends keyof Todo, Value extends number | string | boolean> = TL extends [infer First, ...infer Rest]
+    ? First extends Todo
+        ? First[Key] extends Value
+            ? [First, ...FilterBy<Rest, Key, Value>]
+            : [...FilterBy<Rest, Key, Value>]
+        : []
+    : []
+
+type FilterBy2 = FilterBy<TodoList, 'completed', false>
